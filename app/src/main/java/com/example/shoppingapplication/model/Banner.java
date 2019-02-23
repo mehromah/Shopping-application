079011ba86921773dae2dc8165f439ffb4db6d9a
Design <m.dehdar2018@gmail.com>
2019-02-23 10:48:43 +0330

    third commit

--- app/src/main/java/com/example/shoppingapplication/model/Banner.java
+++ app/src/main/java/com/example/shoppingapplication/model/Banner.java
@@ -1,4 +1,49 @@
+
 package com.example.shoppingapplication.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Banner {
+
+    @SerializedName("id")
+    private Long mId;
+    @SerializedName("image")
+    private String mImage;
+    @SerializedName("link_type")
+    private Long mLinkType;
+    @SerializedName("link_value")
+    private String mLinkValue;
+
+    public Long getId() {
+        return mId;
+    }
+
+    public void setId(Long id) {
+        mId = id;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    public void setImage(String image) {
+        mImage = image;
+    }
+
+    public Long getLinkType() {
+        return mLinkType;
+    }
+
+    public void setLinkType(Long linkType) {
+        mLinkType = linkType;
+    }
+
+    public String getLinkValue() {
+        return mLinkValue;
+    }
+
+    public void setLinkValue(String linkValue) {
+        mLinkValue = linkValue;
+    }
+
 }
