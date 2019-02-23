d8a5f0812f2c4f82d52b8615158b49df3572db24
Design <m.dehdar2018@gmail.com>
2019-02-23 10:59:17 +0330

    third commit

--- app/src/main/java/com/example/shoppingapplication/main/BannerAdapter.java
+++ app/src/main/java/com/example/shoppingapplication/main/BannerAdapter.java
@@ -1,4 +1,52 @@
 package com.example.shoppingapplication.main;
 
-public class BannerAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.shoppingapplication.model.Banner;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BannerAdapter extends RecyclerView.Adapter<BannerAdapter.BannerViewHolder> {
+    private List<Banner> banners = new ArrayList<>();
+
+    public void setBanners(List<Banner> banners) {
+        this.banners = banners;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public BannerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ImageView imageView = new ImageView(parent.getContext());
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        imageView.setAdjustViewBounds(true);
+        return new BannerViewHolder(imageView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BannerViewHolder holder, int position) {
+        Picasso.get().load(banners.get(position).getImage()).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return banners.size();
+    }
+
+    public class BannerViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+
+        public BannerViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView;
+        }
+    }
 }
