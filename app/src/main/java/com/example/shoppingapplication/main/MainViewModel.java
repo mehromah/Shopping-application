193dbb70fb359c66c3bd2be05024885908ddc43a
Design <m.dehdar2018@gmail.com>
2019-02-27 13:09:35 +0330

    third commit

--- app/src/main/java/com/example/shoppingapplication/main/MainViewModel.java
+++ app/src/main/java/com/example/shoppingapplication/main/MainViewModel.java
@@ -1,15 +1,42 @@
 package com.example.shoppingapplication.main;
 
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+
+import com.example.shoppingapplication.data.ShoppingRepository;
 import com.example.shoppingapplication.model.api.ApiService;
 import com.example.shoppingapplication.model.api.JSONUserResponse;
 import com.example.shoppingapplication.model.api.JsonResponse;
 import com.example.shoppingapplication.providers.ApiServiceProvider;
 
-import retrofit2.Call;
+import java.util.List;
 
 public class MainViewModel {
-    private ApiService apiService = ApiServiceProvider.provideApiService();
 
-    Call<JsonResponse> call = apiService.getJSON();
-    Call<JSONUserResponse> callUser = apiService.getUsersJSON();
+    private ShoppingRepository shoppingRepository;
+    private LiveData<List<JsonResponse>> jsonResponse;
+    private LiveData<List<JSONUserResponse>> getJsonUserResponse;
+
+    public MainViewModel(Application application){
+        shoppingRepository = new ShoppingRepository(application);
+        jsonResponse = shoppingRepository.getJsonResponse();
+    }
+
+
+    LiveData<List<JsonResponse>> getJsonResponse() {
+        return jsonResponse;
+    }
+        void saveJsonResponseList(final JsonResponse jsonResponse) {
+        shoppingRepository.insert(jsonResponse);
+    }
+
+
+
+
+
+
+
+
+
+
 }
