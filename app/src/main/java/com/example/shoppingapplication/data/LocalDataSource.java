be850e0985cd036d8a9778729bdddf906894b6c1
Design <m.dehdar2018@gmail.com>
2019-02-27 13:04:14 +0330

    third commit

--- app/src/main/java/com/example/shoppingapplication/data/LocalDataSource.java
+++ app/src/main/java/com/example/shoppingapplication/data/LocalDataSource.java
@@ -1,102 +1,55 @@
 package com.example.shoppingapplication.data;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
-import com.example.shoppingapplication.model.PRSMBL003;
-import com.example.shoppingapplication.model.PRSMBL004;
-import com.example.shoppingapplication.model.PRSMBL005;
-import com.example.shoppingapplication.model.PRSMBL009;
-
+import com.example.shoppingapplication.model.api.JSONUserResponse;
+import com.example.shoppingapplication.model.api.JsonResponse;
 import java.util.List;
 
-import io.reactivex.Single;
-
-//@android.arch.persistence.room.Dao
 @Dao
-public abstract class LocalDataSource implements NewsDataSource {
+public abstract class LocalDataSource implements ShoppingDataSource {
 
-//    @Query("SELECT * FROM tbl_user")
-//    @Override
-//    public abstract Single<List<PRSMBL009>> getUser();
-//
-//
-//    @Query("SELECT * FROM tbl_product")
-//    @Override
-//    public abstract Single<List<Product>> getProduct();
-//
-//    @android.arch.persistence.room.Insert(onConflict = OnConflictStrategy.IGNORE)
-//    public abstract void saveUsersList(List<PRSMBL009> usersList);
-//
-//    @android.arch.persistence.room.Update
+
+//    @Update
 //    public abstract void addUser(PRSMBL009 user);
-//
-//
-//    @android.arch.persistence.room.Query("SELECT * FROM tbl_user WHERE mCMBL009001 LIKE '%' || :keyword || '%'")
-//    @Override
-//    public abstract Single<List<PRSMBL009>> searchUser(String keyword);
-//
-//    @android.arch.persistence.room.Query("DELETE FROM tbl_user")
-//    public abstract void removeAllRows();
+
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    public abstract void savePRSMBL003List(List<PRSMBL003> prsmbl003List);
+    public abstract void saveJsonResponseList(List<JsonResponse> jsonResponseList);
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    public abstract void savePRSMBL004List(List<PRSMBL004> prsmbl004List);
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    public abstract void savePRSMBL005List(List<PRSMBL005> prsmbl005List);
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    public abstract void savePRSMBL009List(List<PRSMBL009> prsmbl009List);
+    public abstract void saveJsonUserResponseList(List<JSONUserResponse> jsonUserResponseList);
 
 
 
-    @Query("SELECT * FROM tbl_PRSMBL003")
+
+
+    @Query("SELECT * FROM tbl_PRSMBL003,tbl_PRSMBL004,tbl_PRSMBL005")
     @Override
-    public Single<List<PRSMBL003>> getPRSMBL003() {
-        return null;
-    }
-
-    @Query("SELECT * FROM tbl_PRSMBL004")
-    @Override
-    public Single<List<PRSMBL004>> getPRSMBL004() {
-        return null;
-    }
-
-    @Query("SELECT * FROM tbl_PRSMBL005")
-    @Override
-    public Single<List<PRSMBL005>> getPRSMBL005() {
+    public LiveData<List<JsonResponse>> getJsonResponse() {
         return null;
     }
 
 
-    @Query("SELECT * FROM tbl_PRSMBL009")
+    @Query("SELECT * FROM tbl_PRSMBL009 ")
     @Override
-    public Single<List<PRSMBL009>> getPRSMBL009() {
+    public LiveData<List<JSONUserResponse>> getJsonUserResponse() {
         return null;
     }
 
 
     @Query("SELECT * FROM tbl_PRSMBL003 WHERE mCMBL003011 LIKE '%' || :keyword || '%'")
     @Override
-    public abstract Single<List<PRSMBL003>> search(String keyword);
+    public abstract LiveData<List<JsonResponse>> search(String keyword);
+
 
     @Query("DELETE FROM tbl_prsmbl003")
     public abstract void removeAllRows();
 
 
-
-
-
-
-
-
-
-
-
-
+    public abstract void insert(JsonResponse param);
 }
