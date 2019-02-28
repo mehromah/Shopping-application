aaecd39bad98753ac192546f5e28ae72a9f9cb1d
Design <m.dehdar2018@gmail.com>
2019-02-28 10:32:50 +0330

    third commit

--- app/src/main/java/com/example/shoppingapplication/model/api/ApiService.java
+++ app/src/main/java/com/example/shoppingapplication/model/api/ApiService.java
@@ -1,5 +1,11 @@
 package com.example.shoppingapplication.model.api;
 
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+import io.reactivex.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
@@ -7,22 +13,13 @@ public interface ApiService {
 
 
     @GET("api/values?url=<NewDataSet><Table><ver>1_02.01.06</ver><proc>001LOCALSELECT</proc><P1>010001</P1><P2></P2><P3></P3><P4>1</P4></Table></NewDataSet>")
-    Call<JsonResponse> getJSON();
+    Flowable<List<JsonResponse>> getJSON();
 
     @GET("api/values?url=<NewDataSet><Table><ver>1_02.01.06</ver><proc>003TOTALSELECT</proc><P1>ADMIN</P1><P2>123456</P2><P3>MBLTYPEVISIT1</P3></Table></NewDataSet>")
-    Call<JSONUserResponse> getUsersJSON();
+    Single<JSONUserResponse> getUsersJSON();
 
 
 
-
-//    @GET("")
-//    Call<Banner> getBanner();
-//
-//    @FormUrlEncoded
-//    @POST("values?url=<NewDataSet><Table><ver>1_02.01.06</ver><proc>003TOTALSELECT</proc><P1> myUsername </P1><P2>myPassword</P2><P3> MBLTYPEVISIT1</P3></Table></NewDataSet>")
-//    Single<PRSMBL009> getUser(
-//            @Field("username") String email,
-//            @Field("password") String password);
 
 
 }
